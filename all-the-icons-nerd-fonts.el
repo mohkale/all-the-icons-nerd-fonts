9373af5e3fdfaf906ec29c673eeaf4f35ab06dbe
Mohsin Kaleem <mohkale@kisara.moe>
2023-02-04 10:02:26 +0000

    Update LICENSE

--- all-the-icons-nerd-fonts.el
+++ all-the-icons-nerd-fonts.el
@@ -1,27 +1,26 @@
 ;;; all-the-icons-nerd-fonts.el --- Nerd font integration for all-the-icons -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021  Mohsin Kaleem
+;; Copyright (C) 2023  Mohsin Kaleem
 
 ;; Author: Mohsin Kaleem <mohkale@gmail.com>
 ;; Keywords: convenience, lisp
 ;; Package-Requires: ((emacs "28.0") (all-the-icons "5.0") (nerd-fonts "0.1"))
 ;; Version: 0.1
 
-;; Copyright (C) 2021 Mohsin Kaleem
+;; This file is part of all-the-icons-nerd-fonts.
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 2
-;; of the License, or (at your option) any later version.
+;; all-the-icons-nerd-fonts is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option) any
+;; later version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; all-the-icons-nerd-fonts is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+;; You should have received a copy of the GNU General Public License along with
+;; all-the-icons-nerd-fonts. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
